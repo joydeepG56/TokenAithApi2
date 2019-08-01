029eb0c585d466ca6f066119adc9226fa044dae5
Joydeep Ghosh <joydeep.56@hotmail.com>
2019-08-01 15:34:59 +0530

    Initial Commit

---
+++ TokenAithApi2/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TokenAithApi2.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using (Test2Entities _db = new Test2Entities())
+            {
+                return _db.Employees.ToList();
+            }
+        }
+    }
+}
