0ecbcb19f5b2901e08185ae48f4fd495450c5a48
Joydeep Ghosh <joydeep.56@hotmail.com>
2019-08-01 15:20:43 +0530

    Add project files.

---
+++ TokenAithApi2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TokenAithApi2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TokenAithApi2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TokenAithApi2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
